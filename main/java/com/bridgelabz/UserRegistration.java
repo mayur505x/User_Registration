2562e6ded5d2fd8798f77f944039be57f5a86282
Mayur505x <mayurpawar8691@gmail.com>
2022-07-20 21:10:54 +0530

    Added: Valid Last name

--- main/java/com/bridgelabz/UserRegistration.java
+++ main/java/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class UserRegistration {
@@ -8,8 +9,12 @@ public class UserRegistration {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a First Name: ");
         String firstName = scan.nextLine();
+        System.out.println("Enter a Last Name: ");
+        String lastName = scan.nextLine();
         UserRegistration.firstNameValidator(firstName);
-        System.out.println("First name is Valid ? " + checkValid(firstNameValidator(firstName))  );
+        System.out.println("First name is Valid ? " + checkValid(firstNameValidator(firstName)));
+        UserRegistration.lastNameValidator(lastName);
+        System.out.println("Last name is Valid ?" + checkValid(lastNameValidator(lastName)));
     }
 
     public static String checkValid(Boolean validator) {
@@ -22,5 +27,9 @@ public class UserRegistration {
 
     public static boolean firstNameValidator(String firstName) {
         return  firstName.matches("[A-Z][a-z]{3,}");
+    }
+
+    public static boolean lastNameValidator(String lastName) {
+        return lastName.matches("[A-Z][a-z]{3,}");
     }
 }
